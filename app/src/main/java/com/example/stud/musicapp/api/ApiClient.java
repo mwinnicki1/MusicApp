66f243cbaaad98d8f662c6f9efd0073e6b9725ef
W57094 <miloszwinnicki@gmail.com>
2018-05-12 13:14:11 +0200

    Retrofit

---
+++ app/src/main/java/com/example/stud/musicapp/api/ApiClient.java
@@ -0,0 +1,6 @@
+package com.example.stud.musicapp.api;
+
+
+
+public interface ApiClient {
+}
---
+++ app/src/main/java/com/example/stud/musicapp/api/ApiService.java
@@ -0,0 +1,16 @@
+package com.example.stud.musicapp.api;
+
+
+import retrofit2.Retrofit;
+
+public class ApiService {
+
+    public static ApiClient getService() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://www.theaudiodb.com/api/v1/json/1/")
+                .build();
+
+        return retrofit.create(ApiClient.class);
+    }
+
+}
