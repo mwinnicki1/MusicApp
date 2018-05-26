2c99d45043f8123d59c84ef3b65d51d091541aa1
W57094 <miloszwinnicki@gmail.com>
2018-05-26 13:02:17 +0200

    pobieranie danych

--- app/src/main/java/com/example/stud/musicapp/MainActivity.java
+++ app/src/main/java/com/example/stud/musicapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.stud.musicapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.stud.musicapp.topsongs.TopSongsActivity;
+import com.example.stud.musicapp.api.ApiService;
+import com.example.stud.musicapp.api.TrendingList;
+import com.google.gson.Gson;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +24,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         Button bTopSongs = findViewById(R.id.bTopSongs);
         bTopSongs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, TopSongsActivity.class);
+                Intent intent = new Intent(MainActivity.this, com.example.stud.musicapp.topsongs.TopSongsActivity.class);
                 startActivity(intent);
             }
         });
